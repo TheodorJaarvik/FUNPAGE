a35748d5c6a2522983f3f34b51a25e8d5a9ea422
TheodorJaarvik <91195433+TheodorJaarvik@users.noreply.github.com>
2021-11-18 23:14:35 +0100

    files

---
+++ script.js
@@ -0,0 +1,33 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+var counter = 0;
+
+function jump(){
+    if(character.classList != "animate"){
+        character.classList.add("animate");
+        counter++;
+        document.getElementById("points").innerHTML=counter;
+        document.getElementById("jump").play();
+    }
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },500);
+}
+
+var checkdead = setInterval(function(){
+    var charactertop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockleft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    
+    if(blockleft < 20 && blockleft > 0 && charactertop >= 220){
+        block.style.animation = "none";
+        block.style.display= "none";
+        character.style.animatiion="none";
+        character.style.display="none";
+        document.getElementById("error2").setAttribute("src","images/error2.png");
+        document.getElementById("points").style.left="38%";
+        document.getElementById("points").style.color="red";
+        document.getElementById("points").innerHTML="GAME OVER";
+        document.getElementById("points2").innerHTML="Your score: " + counter;
+        document.getElementById("audio").play();
+    }
+},10);
